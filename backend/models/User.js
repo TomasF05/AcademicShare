4d18228b91d8b8b9f2d11e0181bd2fddc1c30558
TomasF05 <tomasjf05@gmail.com>
2025-12-26 17:08:23 +0000

    update user.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -8,20 +8,25 @@ const userSchema = mongoose.Schema({
     email: {
         type: String,
         required: [true, 'Por favor, insira um email'],
-        unique: true
+        unique: true,
+        // Regra para validar se termina em @ipvc.pt
+        match: [
+            /^[a-zA-Z0-9._%+-]+@ipvc\.pt$/, 
+            'Por favor, utilize um email institucional @ipvc.pt'
+        ]
     },
     password: {
         type: String,
-        required: [true, 'Por favor, insira uma password']
+        required: [true, 'Por favor, insira uma password'],
+        minlength: [8, 'A password deve ter pelo menos 8 caracteres'] // Validação no MongoDB
     },
     role: {
         type: String,
         enum: ['student', 'admin'],
         default: 'student'
-
     }
 }, { 
-    timestamps: true // adiciona automaticamento campos createdAt e updatedAt. útil para saber quando o user foi criado ou atualizado
+    timestamps: true 
 });
 
 module.exports = mongoose.model('User', userSchema);
